e7bcb2aa5cac646f110d4e06cfdec3fe54cd58b7
Sebastián Vallespir <42224221+dsvallespir@users.noreply.github.com>
2026-06-11 13:49:07 -0300

    patch: env import v3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
 
 
   const env = loadEnv(mode, process.cwd(), '');
