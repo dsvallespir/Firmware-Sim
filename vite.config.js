ccea9a173a21c3fe477f96b3219f8f941583d8d9
Sebastián Vallespir <42224221+dsvallespir@users.noreply.github.com>
2026-06-11 13:45:38 -0300

    patch: env import correction v2

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,33 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // Usar copias locales MIT de las librerías wokwi
-      'avr8js': path.resolve(__dirname, 'wokwi-libs/avr8js/dist/esm'),
-      '@wokwi/elements': path.resolve(__dirname, 'wokwi-libs/wokwi-elements/dist/esm'),
-    },
-  },
-  server: {
-    host: true,   // escucha en 0.0.0.0 → accesible por red local
-    port: 5274,
-    // Proxy para el backend durante desarrollo
-    // Esto evita problemas de CORS: las peticiones a /api
-    // se reenvían automáticamente al backend en :8001
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
+export default defineConfig(() => {
+
+
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        // Usar copias locales MIT de las librerías wokwi
+        'avr8js': path.resolve(__dirname, 'wokwi-libs/avr8js/dist/esm'),
+        '@wokwi/elements': path.resolve(__dirname, 'wokwi-libs/wokwi-elements/dist/esm'),
       },
     },
-  },
-});
+    server: {
+      host: true,   // escucha en 0.0.0.0 → accesible por red local
+      port: 5274,
+      // Proxy para el backend durante desarrollo
+      // Esto evita problemas de CORS: las peticiones a /api
+      // se reenvían automáticamente al backend en :8001
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
+}
+);
